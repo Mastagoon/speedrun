32d51af8f6a8cc8e3e4d7ed3d022ed27db660531
Mastagoon <inzilagiway@gmail.com>
2023-06-09 14:55:31 +0300

    ?

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,51 @@
 import { useEffect, useState } from "react"
 
 export default function Home() {
-	const [days, setDays] = useState(0)
-	const [hours, setHours] = useState(0)
-	const [minutes, setMinutes] = useState(0)
-	const [seconds, setSeconds] = useState(0)
-	const [countdownDate, setcoutndownDate] = useState(new Date("Feb 16, 2023 14:50:00").getTime())
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [countdownDate, setcoutndownDate] = useState(new Date("June 10, 2023 01:30:00").getTime())
 
-	const dateToHoursMinutseSeconds = (date) => {
-		const days = Math.floor(date / (1000 * 60 * 60 * 24))
-		const hours = Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-		const minutes = Math.floor((date % (1000 * 60 * 60)) / (1000 * 60))
-		const seconds = Math.floor((date % (1000 * 60)) / 1000)
+  const dateToHoursMinutseSeconds = (date) => {
+    const days = Math.floor(date / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((date % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((date % (1000 * 60)) / 1000)
 
-		return { days, hours, minutes, seconds }
-	}
+    return { days, hours, minutes, seconds }
+  }
 
-	const addZeroToTheLeft = (number) => {
-		return number < 10 ? `0${number}` : number
-	}
+  const addZeroToTheLeft = (number) => {
+    return number < 10 ? `0${number}` : number
+  }
 
-	useEffect(() => {
-		setInterval(() => {
-			const now = new Date().getTime()
-			const distance = countdownDate - now
-			const { days, hours, minutes, seconds } = dateToHoursMinutseSeconds(distance)
-			setDays(addZeroToTheLeft(days))
-			setHours(addZeroToTheLeft(hours))
-			setMinutes(addZeroToTheLeft(minutes))
-			setSeconds(addZeroToTheLeft(seconds))
-		}, 1000)
-	}, [])
+  useEffect(() => {
+    setInterval(() => {
+      const now = new Date().getTime()
+      const distance = countdownDate - now
+      const { days, hours, minutes, seconds } = dateToHoursMinutseSeconds(distance)
+      setDays(addZeroToTheLeft(days))
+      setHours(addZeroToTheLeft(hours))
+      setMinutes(addZeroToTheLeft(minutes))
+      setSeconds(addZeroToTheLeft(seconds))
+    }, 1000)
+  }, [])
 
-	return <div className="flex h-screen w-screen justify-center items-center">
-		<div class="flex flex-row gap-20">
-			<div className="text-8xl font-bold">
-				{days}d
-			</div>
-			<div className="text-8xl font-bold">
-				{hours}h
-			</div>
-			<div className="text-8xl font-bold">
-				{minutes}m
-			</div>
-			<div className="text-8xl font-bold">
-				{seconds}s
-			</div>
-		</div>
-	</div>
+  return <div className="flex h-screen w-screen justify-center items-center">
+    <div class="flex flex-row gap-20">
+      <div className="text-8xl font-bold">
+        {days}d
+      </div>
+      <div className="text-8xl font-bold">
+        {hours}h
+      </div>
+      <div className="text-8xl font-bold">
+        {minutes}m
+      </div>
+      <div className="text-8xl font-bold">
+        {seconds}s
+      </div>
+    </div>
+  </div>
 }
